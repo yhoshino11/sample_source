2350ac4f02be23f57f875a78210ecc644f37fb11
Yu Hoshino <yhoshino11@gmail.com>
2015-05-07 02:06:11 +0900

    iteration output

--- part2/02/hello.rb
+++ part2/02/hello.rb
@@ -7,3 +7,16 @@ end
 rubies = ['MRI', 'jruby', 'rubinius']
 
 hello(rubies)    # 順番に"HELLO, MRI", "HELLO, JRUBY", "HELLO, RUBINIUS"と表示
+
+require 'rspec'
+
+RSpec.describe 'hello' do
+  it 'says array' do
+    output = <<-EOS
+HELLO, MRI
+HELLO, JRUBY
+HELLO, RUBINIUS
+EOS
+    expect { hello(rubies) }.to output(output).to_stdout
+  end
+end
