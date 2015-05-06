fbdd3a152272d0847f373bcc86f9576595109839
Yu Hoshino <yhoshino11@gmail.com>
2015-05-07 01:52:45 +0900

    Samplepart1/01/list1.1.rbsay => "Hello, worldgit add ."

--- part1/01/list1.1.rb
+++ part1/01/list1.1.rb
@@ -6,3 +6,12 @@ end
 
 sample = Sample.new
 sample.say # Hello, world! と表示
+
+require 'rspec'
+
+RSpec.describe Sample do
+  let(:sample) { Sample.new }
+  it 'says hello world' do
+    expect { sample.say }.to output("Hello, world!\n").to_stdout
+  end
+end
